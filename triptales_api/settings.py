319cbeaee57d2c47658a146e1b0e89072d002a69
Paul Cristian Bordeanu <paul.bordeanu.3d@gmail.com>
2024-07-31 20:37:56 +0000

    Fix 400 bad request error

--- triptales_api/settings.py
+++ triptales_api/settings.py
@@ -56,7 +56,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-paaulcb-triptalesapi-u3ohwk03sth.ws.codeinstitute-ide.net','triptales.herokuapp.com']
+ALLOWED_HOSTS = ['8000-paaulcb-triptalesapi-u3ohwk03sth.ws.codeinstitute-ide.net','https://triptales-e15436dab61c.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-paaulcb-triptalesapi-u3ohwk03sth.ws.codeinstitute-ide.net']
 
